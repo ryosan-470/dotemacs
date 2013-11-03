49abfe3f2de756e6a773317e4dbb84b7ac0cebfa
Ryosuke Sato (COINS) <s1311367@coins.tsukuba.ac.jp>
2013-11-03 12:51:10 +0900

    Modified install script

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 files="
 .emacs.d
 "
-REPO="${PWD}"
+PWD=${PWD}
 echo '==========================================================='
 echo 'W A R N I N G'
 echo 'Now, we will install emacs config to own your pc.'
@@ -18,16 +18,14 @@ if [ "$ans" = "yes" ]
 then
     echo 'Start cloneing to github and making symbolic link...'
     mkdir ${HOME}/.dotconfig
-    cd    ${HOME}/.dotconfig
-    git clone https://github.com/jtwp470/dotemacs.git
     cd ..
+    cp -r dotemacs ${HOME}/.dotconfig/
     ln -s ${HOME}/.dotconfig/dotemacs ${HOME}/.emacs.d 
 else
     echo 'Abort installing'
     exit 0
 fi
 
-EMACS="${HOME}/.dotconfig/dotemacs"
 # Installing emacs elisp
 echo '==========================================================='
 echo 'We will install emacs plugin (emacs elisp) below'
@@ -39,7 +37,7 @@ read ans
 if [ "$ans" = "yes" ]
 then
     # yasnippet
-    cd ${EMACS}/elisp/
+    cd ${HOME}/.dotconfig/dotemacs/elisp/
     echo '==========================================================='
     echo '|installing yasnippet...                                  |'
     echo '==========================================================='
