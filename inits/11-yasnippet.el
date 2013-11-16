af8fbd1d09ecb050fa555d7423cc7bae35587370
Ryosuke Sato(on Ub 13.10) <rskjtwp@gmail.com>
2013-11-16 11:06:16 +0900

    New yasnippet for autocomplete templete for using java and latex

--- inits/11-yasnippet.el
+++ inits/11-yasnippet.el
@@ -1,4 +1,20 @@
 ;; 11-yasnippet.el (Dependences by AutoCompleteJava)
 (add-to-list 'load-path "~/.emacs.d/elisp/yasnippet")
 (require 'yasnippet)
+;; save directory which make snippets
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets"
+        "~/.emacs.d/elisp/yasnippet/s"))
 (yas-global-mode 1)
+
+;; 単語展開キーバインド (ver8.0から明記しないと機能しない)
+;; (setqだとtermなどで干渉問題ありでした)
+;; もちろんTAB以外でもOK 例えば "C-;"とか
+(custom-set-variables '(yas-trigger-key "TAB"))
+
+;; 既存スニペットを挿入する
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+;; 新規スニペットを作成するバッファを用意する
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+;; 既存スニペットを閲覧・編集する
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
