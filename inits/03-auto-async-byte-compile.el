7a46deb567e6eee36e2db027ea7fc17a8e512399
Ryosuke Sato (Ubuntu 13.10) <tango@jtwp470.net>
2014-01-19 11:13:50 +0900

    Add auto-async-byte-compile.el

---
+++ inits/03-auto-async-byte-compile.el
@@ -0,0 +1,4 @@
+;; 03-auto-async-byte-compile.el
+(require 'auto-async-byte-compile)
+(setq auto-async-byte-compile-exclude-files-regexp "/junk/")
+(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
