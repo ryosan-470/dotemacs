9d40a7d12372c7ca7fbd9d22f23cb3da576f15d1
Ryosuke Sato <tango@jtwp470.net>
2014-02-01 15:14:33 +0900

    Enable flycheck for python-mode

--- inits/15-flycheck.el
+++ inits/15-flycheck.el
@@ -1,7 +1,5 @@
 ;; Flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (setq flycheck-check-syntax-automatically '(mode-enabled save))
-;; flycheck-color-mode-line.el
-(require 'flycheck-color-mode-line)
-(eval-after-load "flycheck"
-  '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
+;; Python
+(add-hook 'python-mode-hook 'flycheck-mode)
