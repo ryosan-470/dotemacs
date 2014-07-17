2e1cdb4e8a655ef2d5f9ac9718705aebd71dfbcd
Ryosuke SATO <tango@jtwp470.net>
2014-07-17 14:37:35 +0900

    Add twittering-mode

---
+++ inits/00-basic.el
@@ -0,0 +1,3 @@
+;; 00-basic.el
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'manoj-dark t)
---
+++ inits/33-twittering-mode.el
@@ -0,0 +1,6 @@
+;;; 33-twittering-mode.el
+(setq twittering-use-master-password t)
+(setq twittering-use-ssl t)
+(setq twittering-icon-mode t)             ;; show icons
+(setq twittering-timer-interval 300)      ;; update my timeline each 300 sec
+
