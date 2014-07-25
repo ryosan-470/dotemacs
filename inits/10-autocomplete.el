b23e497ec89ceb0a5f805f91a698d6542ba110f6
Ryosuke Sato <tango@jtwp470.net>
2014-07-25 22:06:30 +0900

    Autocomplete.elのエラー修正

--- inits/10-autocomplete.el
+++ inits/10-autocomplete.el
@@ -1,6 +1,4 @@
 ;;; 10-autocomplete.el
-;;; Dictionary PATH
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
 (setq ac-use-menu-map t)
 (define-key ac-menu-map "C-n" 'ac-next)
@@ -8,3 +6,4 @@
 
 ;; 大文字と小文字を区別
 (custom-set-variables '(ac-ignore-case nil))
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
