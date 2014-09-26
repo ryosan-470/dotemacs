305ac6a8f1fc9ed2d098339abc767c25e6d1ffe1
Ryosuke Sato <tango@jtwp470.net>
2014-09-26 10:41:29 +0900

    Fix rainbow-delimiters error
    We can't use global mode because of software updating

--- inits/27-rainbow-delimiters.el
+++ inits/27-rainbow-delimiters.el
@@ -1,6 +1,10 @@
 ;;; 27-rainbow-delimiters.el --- カッコに対応して色付けする
 ;;; Commentary:
 ;;; Code:
-(require 'rainbow-delimiters)
-(global-rainbow-delimiters-mode)
+(add-hook 'python-mode         'rainbow-delimiters-mode) ;; Python
+(add-hook 'emacs-lisp-mode     'rainbow-delimiters-mode) ;; Emacs Lisp
+(add-hook 'c-mode              'rainbow-delimiters-mode) ;; C
+(add-hook 'java-mode           'rainbow-delimiters-mode) ;; Java
+(add-hook 'shell-script-mode   'rainbow-delimiters-mode) ;; Shell
+(add-hook 'tuareg-mode         'rainbow-delimiters-mode) ;; OCaml
 ;;; 27-rainbow-delimiters.el ends here
