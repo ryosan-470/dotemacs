139ce139024fa223086c57c0a6ac7c96483c5f01
Ryosuke Sato <rskjtwp@gmail.com>
2015-07-02 23:40:08 +0900

    Change suffix number for prog-lang

--- inits/50-python.el
+++ inits/50-python.el
@@ -1,17 +1,8 @@
 ;;; 16-python.el --- For Python configuration
 ;;; Commentary:
 ;;; Code:
-;;; For Python
-;; (autoload 'python-mode "python-mode" nil t)
-;; (add-to-list 'auto-mode-alist '("\\.py$"  . python-mode))
-
-;; ;; indent
 (add-hook 'python-mode-hook
-           '(lambda ()
-;;             (setq indent-tabs-mode nil)
-;;             (setq indent-level 2)
-;;             (setq python-indent 4)
-;;             (setq tab-width 2)
+          '(lambda ()
              (define-key python-mode-map (kbd "TAB") nil)))
 
 ;; 関数一覧の表示
@@ -25,6 +16,6 @@
 ;; (require 'ac-python)
 ;; (add-to-list 'ac-modes 'python-2-mode)
 ;; Jedi.el - Python auto-completion for Emacs
-(add-hook 'python-mode-hook 'jedi:setup)
+(add-hook 'python-mode-hook 'jedi:ac-setup)
 (setq jedi:complete-on-dot t)
 ;;; 16-python.el ends here
